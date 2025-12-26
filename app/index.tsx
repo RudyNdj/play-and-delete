17b1223c5a8741bdac298d0b242bd8fe16c57140
Rudy NDJOUENKEU <nabelrudy@yahoo.fr>
2025-12-26 17:54:38 +0100

    exchange code to access token and handle refresh token and logout

--- app/index.tsx
+++ app/index.tsx
@@ -2,19 +2,25 @@ import { useSpotifyAuth } from "@/hooks/spotify-auth";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const {promptAsync} = useSpotifyAuth();
+  const { login, logout, isAuthenticated, accessToken } = useSpotifyAuth();
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Play and delete</Text>
-      <TouchableOpacity
-      style={styles.button}
-        onPress={() => {
-          console.log("Connect to spotify button pressed");
-          promptAsync();
-        }}
-      >
-        <Text style={styles.buttonText}>Connect to Spotify</Text>
-      </TouchableOpacity>
+      {!isAuthenticated && (
+        <TouchableOpacity style={styles.button} onPress={login}>
+          <Text style={styles.buttonText}>Connect to Spotify</Text>
+        </TouchableOpacity>
+      )}
+
+      {isAuthenticated && (
+        <>
+          <Text>Connected to Spotify</Text>
+          <Text>Token preview: {accessToken?.slice(0, 10)}...</Text>
+          <TouchableOpacity style={styles.button} onPress={logout}>
+            <Text style={styles.buttonText}>Logout</Text>
+          </TouchableOpacity>
+        </>
+      )}
     </View>
   );
 }
--- hooks/spotify-auth.ts
+++ hooks/spotify-auth.ts
@@ -1,31 +1,141 @@
 import {
-  AuthRequestConfig,
+  DiscoveryDocument,
+  exchangeCodeAsync,
   makeRedirectUri,
+  refreshAsync,
+  TokenResponse,
   useAuthRequest,
+  useAutoDiscovery,
 } from "expo-auth-session";
-import { useEffect } from "react";
+import * as SecureStore from "expo-secure-store";
+import { useEffect, useState } from "react";
 
 const SCOPES = ["playlist-modify-private", "playlist-modify-public"];
-
-const REDIRECT_URI = makeRedirectUri();
+const STORAKE_KEY = {
+  ACCESS_TOKEN: "SPOTIFY_ACCESS_TOKEN",
+  REFRESH_TOKEN: "SPOTIFY_REFRESH_TOKEN",
+  EXPIRES_AT: "SPOTIFY_EXPIRES_AT",
+};
 
 export function useSpotifyAuth() {
-  const authRequestConfig: AuthRequestConfig = {
-    responseType: "code",
-    clientId: process.env.EXPO_PUBLIC_SPOTIFY_CLIENT_ID!,
-    scopes: SCOPES,
-    redirectUri: REDIRECT_URI,
-  };
+  const discovery = useAutoDiscovery("https://accounts.spotify.com/");
+  const [accessToken, setAccessToken] = useState<string | null>(null);
 
-  const [request, response, promptAsync] = useAuthRequest(authRequestConfig, {
-    authorizationEndpoint: "https://accounts.spotify.com/authorize",
-  });
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: process.env.EXPO_PUBLIC_SPOTIFY_CLIENT_ID!,
+      scopes: SCOPES,
+      redirectUri: makeRedirectUri(),
+    },
+    discovery
+  );
 
+  // When spotify auth response is received
   useEffect(() => {
-    console.log("Auth Request:", request);
-    console.log(request?.url)
-    console.log("Auth Response:", response);
-  }, [request, response]);
+    async function handleAuthResponse() {
+      if (response?.type === "success") {
+        // The user has successfully authenticated
+        // Now we can exchange the authorization code for an access token
+        const { code } = response.params;
 
-  return { request, response, promptAsync };
+        const tokenReponse = await exchangeCodeAsync(
+          {
+            code,
+            clientId: process.env.EXPO_PUBLIC_SPOTIFY_CLIENT_ID!,
+            redirectUri: request?.redirectUri!,
+            extraParams: {
+              code_verifier: request?.codeVerifier!,
+            },
+          },
+          discovery!
+        );
+
+        // Store the tokens securely
+        await storeTokenData(tokenReponse);
+        setAccessToken(tokenReponse.accessToken);
+      }
+    }
+    handleAuthResponse();
+  }, [response]);
+
+  // Load stored access token on mount
+  useEffect(() => {
+    async function loadToken() {
+      if (!discovery) return;
+      const token = await getSpotifyAccessToken(discovery);
+      if (token) setAccessToken(token);
+    }
+    loadToken();
+  }, [discovery]);
+
+  return {
+    login: () => promptAsync(),
+    logout: async () => {
+      await logoutSpotify();
+      setAccessToken(null);
+    },
+    accessToken,
+    isAuthenticated: !!accessToken,
+  };
+}
+
+async function logoutSpotify() {
+  await Promise.all(
+    Object.values(STORAKE_KEY).map((key) => SecureStore.deleteItemAsync(key))
+  );
+}
+
+export async function getSpotifyAccessToken(discovery: DiscoveryDocument) {
+  const tokenData = await getStoredToken();
+
+  if (tokenData.accessToken && tokenData.expiresAt) {
+    const now = Date.now();
+    if (now < tokenData.expiresAt) {
+      // Token is still valid
+      return tokenData.accessToken;
+    }
+    // Token has expired, we have to refresh it
+    const newTokenResponse = await refreshAsync(
+      {
+        clientId: process.env.EXPO_PUBLIC_SPOTIFY_CLIENT_ID!,
+        refreshToken: tokenData.refreshToken!,
+      },
+      discovery
+    );
+
+    // Store the new token data
+    await storeTokenData(newTokenResponse);
+    return newTokenResponse.accessToken;
+  }
+
+  return null;
+}
+
+async function storeTokenData(tokenData: TokenResponse) {
+  await SecureStore.setItemAsync(
+    STORAKE_KEY.ACCESS_TOKEN,
+    tokenData.accessToken!
+  );
+
+  await SecureStore.setItemAsync(
+    STORAKE_KEY.REFRESH_TOKEN,
+    tokenData.refreshToken!
+  );
+
+  const expiresAt = (Date.now() + tokenData.expiresIn! * 1000).toString();
+  await SecureStore.setItemAsync(STORAKE_KEY.EXPIRES_AT, expiresAt);
+}
+
+async function getStoredToken() {
+  const [accessToken, refreshToken, expiresAt] = await Promise.all([
+    SecureStore.getItemAsync(STORAKE_KEY.ACCESS_TOKEN),
+    SecureStore.getItemAsync(STORAKE_KEY.REFRESH_TOKEN),
+    SecureStore.getItemAsync(STORAKE_KEY.EXPIRES_AT),
+  ]);
+
+  return {
+    accessToken,
+    refreshToken,
+    expiresAt: expiresAt ? Number(expiresAt) : null,
+  };
 }
